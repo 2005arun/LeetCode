b9dc7d9e90bf266fade4ded8d8e99119fb722bfc
2005arun <152052006+2005arun@users.noreply.github.com>
2024-11-19 23:53:58 +0530

    Time: 8 ms (54.77%), Space: 126.7 MB (54.87%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>p;
-        vector<int> n;
+        // vector<int>p;
+        // vector<int> n;
+        // for(auto i:nums){
+        //     if(i<0) n.push_back(i);
+        //     else p.push_back(i);
+        // }
+        // vector<int> res;
+        // int l=0,r=0;
+        // while(l<p.size() && r<n.size()){
+        //     res.push_back(p[l]);
+        //     res.push_back(n[r]);
+        //     l++;
+        //     r++;
+        // }
+        // while(l<p.size()){
+        //     res.push_back(p[l]);
+        //     l++;
+        // }
+        // while(r<n.size()){
+        //     res.push_back(n[r]);
+        //     r++;
+        // }
+        // return res;
+        vector<int> vec(nums.size());
+        int p=0,n=1;
         for(auto i:nums){
-            if(i<0) n.push_back(i);
-            else p.push_back(i);
+            if(i>0){
+                vec[p]=i;
+                p+=2;
+            }
+            else{
+                vec[n]=i;
+                n+=2;
+            }
         }
-        vector<int> res;
-        int l=0,r=0;
-        while(l<p.size() && r<n.size()){
-            res.push_back(p[l]);
-            res.push_back(n[r]);
-            l++;
-            r++;
-        }
-        while(l<p.size()){
-            res.push_back(p[l]);
-            l++;
-        }
-        while(r<n.size()){
-            res.push_back(n[r]);
-            r++;
-        }
-        return res;
+        return vec;
     }
 };
