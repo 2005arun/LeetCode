67c8d5dc266680260f546dd02085d7259d95c850
2005arun <152052006+2005arun@users.noreply.github.com>
2024-11-21 22:03:36 +0530

    Time: 0 ms (100.00%), Space: 9 MB (12.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>> vec(matrix.size(),vector<int>(matrix[0].size()));
-        int x=0;
-        for(int j=0;j<matrix[0].size();j++){
-            int y=0;
-            for(int i=matrix.size()-1;i>=0;i--){
-                vec[x][y]=matrix[i][j];
-                y++;
+        // vector<vector<int>> vec(matrix.size(),vector<int>(matrix[0].size()));
+        // int x=0;
+        // for(int j=0;j<matrix[0].size();j++){
+        //     int y=0;
+        //     for(int i=matrix.size()-1;i>=0;i--){
+        //         vec[x][y]=matrix[i][j];
+        //         y++;
+        //     }
+        //     x++;
+        // }
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         matrix[i][j]=vec[i][j];
+        //     }
+        // }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix[0].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
-            x++;
         }
         for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                matrix[i][j]=vec[i][j];
-            }
+            reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
