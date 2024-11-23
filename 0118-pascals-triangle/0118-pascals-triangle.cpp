e92a33efe1e93e6dcff7f664f09cb2fd1d7a0741
2005arun <152052006+2005arun@users.noreply.github.com>
2024-11-23 13:48:20 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (39.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    int ncr(int n,int r){
+    // int ncr(int n,int r){
+    //     int ans=1;
+    //     for(int i=0;i<r;i++){
+    //         ans=ans*(n-i);
+    //         ans=ans/(i+1);
+    //     }
+    //     return ans;
+    // }
+    vector<int> find(int r){
+        vector<int> vec;
         int ans=1;
-        for(int i=0;i<r;i++){
-            ans=ans*(n-i);
-            ans=ans/(i+1);
+        vec.push_back(1);
+        for(int i=1;i<r;i++){
+            ans=ans*(r-i);
+            ans=ans/i;
+            vec.push_back(ans);
         }
-        return ans;
+        return vec;
     }
     vector<vector<int>> generate(int numRows) {
+        // vector<vector<int>> res;
+        // for(int i=1;i<=numRows;i++){
+        //     vector<int> vec;
+        //     for(int j=1;j<=i;j++){
+        //         vec.push_back(ncr(i-1,j-1));
+        //     }
+        //     res.push_back(vec);
+        // }
+        // return res;
         vector<vector<int>> res;
         for(int i=1;i<=numRows;i++){
-            vector<int> vec;
-            for(int j=1;j<=i;j++){
-                vec.push_back(ncr(i-1,j-1));
-            }
-            res.push_back(vec);
+            res.push_back(find(i));
         }
         return res;
     }
