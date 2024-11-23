7b801ab4456c8ab480b93fe2247e9f18e1f20c39
2005arun <152052006+2005arun@users.noreply.github.com>
2024-11-23 18:11:08 +0530

    Time: 22 ms (62.90%), Space: 16.1 MB (71.56%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -35,5 +35,22 @@ public:
             }
         }
         return ans;
+        // set<vector<int>> st;
+        // for(int i=0;i<nums.size();i++){
+        //     for(int j=i+1;j<nums.size();j++){
+        //         set<int> hash;
+        //         for(int k=j+1;k<nums.size();k++){
+        //             long long four=target-(nums[i]+nums[j]+nums[k]);
+        //             if(hash.find(four)!=hash.end()){
+        //                 vector<int> temp={nums[i],nums[j],nums[k],four};
+        //                 sort(temp.begin(),temp.end());
+        //                 st.insert(temp);
+        //             }
+        //             hash.insert(nums[k]);
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> res(st.begin(),st.end());
+        // return res;
     }
 };
