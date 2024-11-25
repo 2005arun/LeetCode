9b15c520c893f1050cd91d2d4cefec1552ea76f1
2005arun <152052006+2005arun@users.noreply.github.com>
2024-11-25 21:09:15 +0530

    Time: 11 ms (17.32%), Space: 23 MB (43.20%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,17 +3,26 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> res;
         sort(intervals.begin(),intervals.end());
+        // for(int i=0;i<intervals.size();i++){
+        //     int a=intervals[i][0];
+        //     int b=intervals[i][1];
+        //     if(!res.empty() && b<=res.back()[1]){
+        //         continue;
+        //     }
+        //     for(int j=i+1;j<intervals.size();j++){
+        //         if(intervals[j][0]<=b) b=max(b,intervals[j][1]);
+        //         else break;
+        //     }
+        //     res.push_back({a,b});
+        // }
+        // return res;
         for(int i=0;i<intervals.size();i++){
-            int a=intervals[i][0];
-            int b=intervals[i][1];
-            if(!res.empty() && b<=res.back()[1]){
-                continue;
+            if(res.empty() || res.back()[1]<intervals[i][0]){
+                res.push_back(intervals[i]);
             }
-            for(int j=i+1;j<intervals.size();j++){
-                if(intervals[j][0]<=b) b=max(b,intervals[j][1]);
-                else break;
+            else{
+                res.back()[1]=max(res.back()[1],intervals[i][1]);
             }
-            res.push_back({a,b});
         }
         return res;
     }
