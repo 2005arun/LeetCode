8c16ff9d637a748e8625c0e65705364a9dc005bb
2005arun <152052006+2005arun@users.noreply.github.com>
2024-12-09 22:34:03 +0530

    Time: 0 ms (100.00%), Space: 62.9 MB (11.00%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,15 +2,6 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n=height.size();
-        // stack<int> st;
-        // vector<int> arr(n,-1);
-        // for(int i=n-1;i>=0;i--){
-        //     while(!st.empty() && height[i]>st.top()){
-        //         st.pop();
-        //     }
-        //     if(!st.empty()) arr[i]=st.top();
-        //     st.push(height[i]);
-        // }
         int l=0,r=n-1;
         int total=0;
         while(l<r){
