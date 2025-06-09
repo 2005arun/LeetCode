7c679c138cddd0ba3c7db506847c10180e999adf
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-06-09 18:18:42 +0530

    Time: 1 ms (59.46%), Space: 15 MB (34.61%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
+    vector<int> vec;
+    void recur(int i,int n){
+        if(i>n) return;
+        vec.push_back(i);
+        for(int ind=0;ind<=9;ind++){
+            if(i*10+ind >n) break;
+            recur(i*10+ind,n);
+        }
+        return;
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string> vec;
-        vector<int> ve;
-        for(int i=1;i<=n;i++){
-            vec.push_back(to_string(i));
+        // vector<string> vec;
+        // vector<int> ve;
+        // for(int i=1;i<=n;i++){
+        //     vec.push_back(to_string(i));
+        // }
+        // sort(vec.begin(),vec.end());
+        // for(auto it:vec){
+        //     ve.push_back(stoi(it));
+        // }
+        // return ve;
+        for(int i=1;i<=9;i++){
+            if(i>n) break;
+            recur(i,n);
         }
-        sort(vec.begin(),vec.end());
-        for(auto it:vec){
-            ve.push_back(stoi(it));
-        }
-        return ve;
+        return vec;
     }
 };
