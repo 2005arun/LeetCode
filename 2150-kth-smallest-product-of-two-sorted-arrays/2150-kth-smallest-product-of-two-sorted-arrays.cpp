8adf4daf095e5f9cd300d2da8199a930a8f8bf5b
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-06-26 08:42:53 +0530

    Time: 593 ms (43.1%), Space: 97.9 MB (98.56%) - LeetHub

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -0,0 +1,48 @@
+#define ll long long
+class Solution {
+public:
+    bool ischeck(ll mid,vector<int>& nums1, vector<int>& nums2, ll k){
+        ll cnt=0;
+        for(int i=0;i<nums1.size();i++){
+            if(nums1[i]>0){
+                ll l=0,r=nums2.size()-1;
+                while(l<=r){
+                    ll m=l+(r-l)/2;
+                    if((ll)nums1[i]*nums2[m] <= mid){
+                        l=m+1;
+                    }
+                    else r=m-1;
+                }
+                cnt+=l;
+            }
+            else if(nums1[i]<0){
+                ll l=-1,r=nums2.size();
+                while(l+1<r){
+                    ll m=l+(r-l)/2;
+                    if((ll)nums1[i]*nums2[m] > mid){
+                        l=m;
+                    }
+                    else r=m;
+                }
+                cnt+=nums2.size()-r;
+            }
+            else{
+                if(0<=mid){
+                    cnt+=nums2.size();
+                }
+            }
+        }
+        return cnt>=k;
+    }
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        ll left=-1e10,right=1e10;
+        while(left<=right){
+            ll mid=left+(right-left)/2;
+            if(ischeck(mid,nums1,nums2,k)){
+                right=mid-1;
+            }
+            else left=mid+1;
+        }
+        return left;
+    }
+};
