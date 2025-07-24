8b0d9d20ddde6d4548feeacb0e9f9a07a46b255a
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-07-24 17:38:18 +0530

    Time: 1244 ms (8.14%), Space: 291.6 MB (10.81%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& vec) {
         // set<vector<int>> st;
         // for(int i=0;i<nums.size();i++){
         //     set<int> hashset;
@@ -16,27 +16,49 @@ public:
         // }
         // vector<vector<int>> res(st.begin(),st.end());
         // return res;
-        vector<vector<int>> vec;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1,k=nums.size()-1;
-            while(j<k){
-                if((nums[i]+nums[j]+nums[k])==0){
-                    vector<int> temp={nums[i],nums[j],nums[k]};
-                    vec.push_back(temp);
-                    j++,k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
+
+        // vector<vector<int>> vec;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //     if(i>0 && nums[i]==nums[i-1]) continue;
+        //     int j=i+1,k=nums.size()-1;
+        //     while(j<k){
+        //         if((nums[i]+nums[j]+nums[k])==0){
+        //             vector<int> temp={nums[i],nums[j],nums[k]};
+        //             vec.push_back(temp);
+        //             j++,k--;
+        //             while(j<k && nums[j]==nums[j-1]) j++;
+        //             while(j<k && nums[k]==nums[k+1]) k--;
+        //         }
+        //         else if((nums[i]+nums[j]+nums[k])<0){
+        //             j++;
+        //         }
+        //         else{
+        //             k--;
+        //         }
+        //     }
+        // }
+        // return vec;
+
+        set<vector<int>> res;
+        sort(vec.begin(),vec.end());
+        for(int i=0;i<vec.size()-2;i++){
+            int v=-vec[i];
+            int l=i+1,r=vec.size()-1;
+            while(l<r){
+                int sum=vec[l]+vec[r];
+                if(sum==v){
+                    res.insert({vec[i],vec[l],vec[r]});
+                    l++,r--;
                 }
-                else if((nums[i]+nums[j]+nums[k])<0){
-                    j++;
+                else if(sum<v){
+                    l++;
                 }
-                else{
-                    k--;
-                }
+                else r--;
             }
         }
-        return vec;
+        vector<vector<int>> out;
+        out.insert(out.end(),res.begin(),res.end());
+        return out;
     }
 };
