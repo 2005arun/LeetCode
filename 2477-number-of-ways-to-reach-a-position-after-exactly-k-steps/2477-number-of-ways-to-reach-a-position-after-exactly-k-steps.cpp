923f01b3ffdd18265ce3a95c76a5aeab73c792ef
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-07-27 19:51:48 +0530

    Time: 166 ms (29.08%), Space: 179.2 MB (25%) - LeetHub

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/2477-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int mod=1e9+7;
-    int offset=0;
     vector<vector<int>> dp;
     int recur(int start,int end,int k,int step){
         if(step==k) {
@@ -14,7 +13,7 @@ public:
         return dp[start+1000][step]=(front+back)%mod;
     }
     int numberOfWays(int startPos, int endPos, int k) {
-        dp.assign(3000,vector<int>(k+1,-1));
+        dp.assign(3000+1,vector<int>(k+1,-1));
         return recur(startPos,endPos,k,0)%mod;
     }
 };
