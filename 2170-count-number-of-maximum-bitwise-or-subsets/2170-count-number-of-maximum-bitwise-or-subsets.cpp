1df24832936fe1a9322da17e081863e9413c582b
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-07-28 11:06:18 +0530

    Time: 105 ms (19.81%), Space: 310.1 MB (10.75%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -11,18 +11,21 @@ public:
     //     subset(ind+1,nums,res,vec);
     //     return ;
     // }
+    vector<vector<int>> dp;
     int recur(int i,vector<int> &nums,int maxm,int x){
         if(i==nums.size()){
             return x==maxm;
         }
+        if(dp[i][x]!=-1) return dp[i][x];
         int non_take=recur(i+1,nums,maxm,x);
         int take=recur(i+1,nums,maxm,x|nums[i]);
-        return take+non_take;
+        return dp[i][x]=take+non_take;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         // vector<vector<int>> res;
         // vector<int> vec;
         // subset(0,nums,res,vec);
+        int n=nums.size();
         int maxm=0;
         for(auto i:nums){
             maxm|=i;
@@ -36,6 +39,7 @@ public:
         //     if(x==maxm) cnt++;
         // }
         // return cnt;
+        dp.assign(n,vector<int>(maxm+1,-1));
         int x=0;
         return recur(0,nums,maxm,x);
     }
