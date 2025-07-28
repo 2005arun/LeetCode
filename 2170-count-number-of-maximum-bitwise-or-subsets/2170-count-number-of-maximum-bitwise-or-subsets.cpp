7e60da876f33141ab4d08e65a3b95dc4757ff17f
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-07-28 11:04:05 +0530

    Time: 7 ms (92.39%), Space: 10.8 MB (43.96%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    void subset(int ind,vector<int> &nums,vector<vector<int>> &res,vector<int> &vec){
-        if(ind==nums.size()){
-            res.push_back(vec);
-            return ;
+    // void subset(int ind,vector<int> &nums,vector<vector<int>> &res,vector<int> &vec){
+    //     if(ind==nums.size()){
+    //         res.push_back(vec);
+    //         return ;
+    //     }
+    //     vec.push_back(nums[ind]);
+    //     subset(ind+1,nums,res,vec);
+    //     vec.pop_back();
+    //     subset(ind+1,nums,res,vec);
+    //     return ;
+    // }
+    int recur(int i,vector<int> &nums,int maxm,int x){
+        if(i==nums.size()){
+            return x==maxm;
         }
-        vec.push_back(nums[ind]);
-        subset(ind+1,nums,res,vec);
-        vec.pop_back();
-        subset(ind+1,nums,res,vec);
-        return ;
+        int non_take=recur(i+1,nums,maxm,x);
+        int take=recur(i+1,nums,maxm,x|nums[i]);
+        return take+non_take;
     }
     int countMaxOrSubsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> vec;
-        subset(0,nums,res,vec);
+        // vector<vector<int>> res;
+        // vector<int> vec;
+        // subset(0,nums,res,vec);
         int maxm=0;
         for(auto i:nums){
             maxm|=i;
         }
-        int cnt=0;
-        for(auto i:res){
-            int x=0;
-            for(auto j:i){
-                x|=j;
-            }
-            if(x==maxm) cnt++;
-        }
-        return cnt;
+        // int cnt=0;
+        // for(auto i:res){
+        //     int x=0;
+        //     for(auto j:i){
+        //         x|=j;
+        //     }
+        //     if(x==maxm) cnt++;
+        // }
+        // return cnt;
+        int x=0;
+        return recur(0,nums,maxm,x);
     }
 };
