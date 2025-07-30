5c90523807a6426fe33b9e07c8813be93d41cbdd
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-07-31 01:13:13 +0530

    Time: 7 ms (59.32%), Space: 13.3 MB (34.5%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -17,7 +17,24 @@ public:
     }
     int minDistance(string word1, string word2) {
         int n=word1.size(),m=word2.size();
-        dp.assign(n+1,vector<int>(m+1,-1));
-        return recur(n-1,m-1,word1,word2);
+        // dp.assign(n+1,vector<int>(m+1,-1));
+        // return recur(n-1,m-1,word1,word2);
+
+        dp.assign(n+1,vector<int>(m+1,0));
+        dp[0][0]=0;
+        for(int i=1;i<=n;i++) dp[i][0]=i;
+        for(int j=1;j<=m;j++) dp[0][j]=j;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else{
+                    int insert=1+dp[i][j-1];
+                    int del=1+dp[i-1][j];
+                    int replace=1+dp[i-1][j-1];
+                    dp[i][j]=min({insert,del,replace});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
