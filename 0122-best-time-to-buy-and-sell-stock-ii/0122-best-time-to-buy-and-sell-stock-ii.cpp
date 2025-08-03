050169fc2bf0087731395d03c0015c146b13f79e
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-03 10:19:40 +0530

    Time: 6 ms (25.51%), Space: 19.7 MB (35.66%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -14,7 +14,25 @@ public:
         return dp[ind][buy]=maxprofit;
     }
     int maxProfit(vector<int>& prices) {
-        dp.assign(prices.size(),vector<int>(2,-1));
-        return recur(0,prices,1);
+        // dp.assign(prices.size(),vector<int>(2,-1));
+        // return recur(0,prices,1);
+        int n=prices.size();
+        dp.assign(n+1,vector<int>(2));  
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                int maxprofit=0;
+                if(j){
+                    maxprofit=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else{
+                    maxprofit=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+                }
+                dp[i][j]=maxprofit;
+            }
+        }     
+        // cout<<dp[0][0]<<" "<<dp[0][1]<<" "; 
+        return dp[0][1];
     }
 };
