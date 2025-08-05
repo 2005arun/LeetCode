304fc670423a532da231e719eb4d0df8c107f8f1
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-05 21:32:54 +0530

    Time: 3 ms (40.8%), Space: 115.6 MB (56.74%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int> temp;
-        temp.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            if(temp.size()==3) return true;
-            if(temp.back()<nums[i]) temp.push_back(nums[i]);
-            else{
-                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-                temp[ind]=nums[i];
+        // vector<int> temp;
+        // temp.push_back(nums[0]);
+        // for(int i=1;i<nums.size();i++){
+        //     if(temp.size()==3) return true;
+        //     if(temp.back()<nums[i]) temp.push_back(nums[i]);
+        //     else{
+        //         int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+        //         temp[ind]=nums[i];
+        //     }
+        // }
+        // if(temp.size()==3) return true;
+        // return false;
+
+        int f=INT_MIN,s=INT_MIN,t=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(f!=INT_MIN && s!=INT_MIN && t!=INT_MIN) return true;
+            if(f==INT_MIN) f=nums[i];
+            else if(s==INT_MIN){
+                if(f<nums[i]) s=nums[i];
+                else f=nums[i];
+            }
+            else if(t==INT_MIN){
+                if(f<nums[i] && s<nums[i]) t=nums[i];
+                else if(f<nums[i] && s>nums[i]) s=nums[i];
+                else if(f>nums[i]) f=nums[i];
             }
         }
-        if(temp.size()==3) return true;
+        if(f!=INT_MIN && s!=INT_MIN && t!=INT_MIN) return true;
         return false;
     }
 };
