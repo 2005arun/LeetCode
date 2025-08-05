a6f0aac055caccdd8efef6280e2bf787c54c62e0
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-05 21:07:14 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int recur(int ind,vector<int> &nums,int n,int prevind){
-        if(ind==n) return 0;
-        if(dp[ind][prevind+1]!=-1) return dp[ind][prevind+1];
-        int take=0;
-        if(prevind==-1 || nums[ind]>nums[prevind]) take=1+recur(ind+1,nums,n,ind);
-        int non_take=recur(ind+1,nums,n,prevind);
-        return dp[ind][prevind+1]=max(take,non_take);
-    }
+    // vector<vector<int>> dp;
+    // int recur(int ind,vector<int> &nums,int n,int prevind){
+    //     if(ind==n) return 0;
+    //     if(dp[ind][prevind+1]!=-1) return dp[ind][prevind+1];
+    //     int take=0;
+    //     if(prevind==-1 || nums[ind]>nums[prevind]) take=1+recur(ind+1,nums,n,ind);
+    //     int non_take=recur(ind+1,nums,n,prevind);
+    //     return dp[ind][prevind+1]=max(take,non_take);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         // dp.assign(n,vector<int>(n+1,-1));
@@ -24,35 +24,50 @@ public:
         //     }
         // }
 
-        vector<int> dp(n);
-        int res=1;
-        vector<int> hash(n);
-        int lastind=0;
-        for(int i=0;i<n;i++){
-            hash[i]=i;
-            int maxm=0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i] && dp[i]<1+dp[j]){
-                    dp[i]=1+dp[j];
-                    hash[i]=j;
-                    maxm=max(res,1+dp[i]);
-                }
-            }
-            if(maxm>res){
-                res=maxm;
-                lastind=i;
-            }
-        }
-        vector<int> out;
+        // print the longest increasing subsequence.
+        // vector<int> dp(n);
+        // int res=1;
+        // vector<int> hash(n);
+        // int lastind=0;
+        // for(int i=0;i<n;i++){
+        //     hash[i]=i;
+        //     int maxm=0;
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i] && dp[i]<1+dp[j]){
+        //             dp[i]=1+dp[j];
+        //             hash[i]=j;
+        //             maxm=max(res,1+dp[i]);
+        //         }
+        //     }
+        //     if(maxm>res){
+        //         res=maxm;
+        //         lastind=i;
+        //     }
+        // }
+        // vector<int> out;
+        // // out.push_back(nums[lastind]);
+        // while(hash[lastind]!=lastind){
+        //     out.push_back(nums[lastind]);
+        //     lastind=hash[lastind];
+        // }
         // out.push_back(nums[lastind]);
-        while(hash[lastind]!=lastind){
-            out.push_back(nums[lastind]);
-            lastind=hash[lastind];
-        }
-        out.push_back(nums[lastind]);
-        reverse(out.begin(),out.end());
-        for(auto i:out) cout<<i<<" ";
-        return res;
+        // reverse(out.begin(),out.end());
+        // for(auto i:out) cout<<i<<" ";
+        // return res;
         // return dp[0][0];
+
+        // Binary Search
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            // cout<<temp[i-1]<<" ";
+            if(temp.back()<nums[i]) temp.push_back(nums[i]);
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
+                // cout<<ind<<" ";
+            }
+        }
+        return temp.size();
     }
 };
