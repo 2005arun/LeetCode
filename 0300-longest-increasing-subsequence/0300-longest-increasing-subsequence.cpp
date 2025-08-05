ba60eea42c5c723f1bad9caf7a323fb4a5eca371
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-05 20:47:47 +0530

    Time: 83 ms (50.51%), Space: 14.3 MB (64.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,15 +14,27 @@ public:
         // dp.assign(n,vector<int>(n+1,-1));
         // return recur(0,nums,n,-1);
 
-        dp.assign(n+1,vector<int>(n+1,0));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int prevind=ind-1;prevind>=-1;prevind--){
-                int take=0;
-                if(prevind==-1 || nums[ind]>nums[prevind]) take=1+dp[ind+1][ind+1];
-                int non_take=dp[ind+1][prevind+1];
-                dp[ind][prevind+1]=max(take,non_take);
+        // dp.assign(n+1,vector<int>(n+1,0));
+        // for(int ind=n-1;ind>=0;ind--){
+        //     for(int prevind=ind-1;prevind>=-1;prevind--){
+        //         int take=0;
+        //         if(prevind==-1 || nums[ind]>nums[prevind]) take=1+dp[ind+1][ind+1];
+        //         int non_take=dp[ind+1][prevind+1];
+        //         dp[ind][prevind+1]=max(take,non_take);
+        //     }
+        // }
+
+        vector<int> dp(n);
+        int res=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i] && dp[i]<1+dp[j]){
+                    dp[i]=1+dp[j];
+                    res=max(res,1+dp[i]);
+                }
             }
         }
-        return dp[0][0];
+        return res;
+        // return dp[0][0];
     }
 };
