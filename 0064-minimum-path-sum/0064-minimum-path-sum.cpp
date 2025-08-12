dede59fbaf7674697c79bdeb4dd758cd85be42a4
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-12 11:11:00 +0530

    Time: 0 ms (100%), Space: 16.2 MB (61.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -12,19 +12,28 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        dp.assign(n,vector<int>(m,-1));
-        return recur(0,0,n,m,grid);
+        // dp.assign(n,vector<int>(m,-1));
+        // return recur(0,0,n,m,grid);
 
-        // dp.assign(n+1,vector<int>(m+1));
-        // dp[0][0]=grid[0][0];
+        dp.assign(n+1,vector<int>(m+1,0));
+        dp[0][0]=grid[0][0];
 
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //         int left=dp[i-1][j];
-        //         int right=dp[i][j-1];
-        //         dp[i][j]=grid[i][j]+min(left,right);
-        //     }
-        // }
-        // return dp[n-1][m-1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) continue;
+                if(i==0){
+                    dp[i][j]=dp[i][j-1]+grid[i][j];
+                    continue;
+                }
+                if(j==0){
+                    dp[i][j]=dp[i-1][j]+grid[i][j];
+                    continue;
+                }
+                int left=dp[i-1][j];
+                int right=dp[i][j-1];
+                dp[i][j]=grid[i][j]+min(left,right);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
