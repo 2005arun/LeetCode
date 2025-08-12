960bbcb9471d53fa4b945e74af1d574e770fd038
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-12 21:04:54 +0530

    Time: 103 ms (66.56%), Space: 14 MB (27.81%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -13,7 +13,20 @@ public:
     int maxCoins(vector<int>& nums) {
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        dp.assign(nums.size(),vector<int>(nums.size(),-1));
-        return recur(1,nums.size()-2,nums);
+        // dp.assign(nums.size(),vector<int>(nums.size(),-1));
+        // return recur(1,nums.size()-2,nums);
+
+        dp.assign(nums.size(),vector<int>(nums.size(),0));
+        for(int i=nums.size()-2;i>=1;i--){
+            for(int j=1;j<=nums.size()-2;j++){
+                if(i>j) continue;
+                int tot=INT_MIN;
+                for(int k=i;k<=j;k++){
+                    tot=max(tot,(nums[i-1]*nums[k]*nums[j+1])+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=tot;
+            }
+        }
+        return dp[1][nums.size()-2];
     }
 };
