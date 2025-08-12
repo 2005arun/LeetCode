17fa68bb9d2132c62164a2f274cbf890cf90b5f7
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-12 11:04:51 +0530

    Time: 511 ms (16.99%), Space: 385.3 MB (5.88%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -13,5 +13,19 @@ public:
     int numberOfWays(int n, int x) {
         dp.assign(300+1,vector<int>(300+1,-1));
         return recur(1,n,x,0);
+        
+        // dp.assign(n+1,vector<int>(n+1,0));
+        // for(int i=0;i<n+1;i++){
+        //     dp[i][n]=1;
+        // }
+        // for(int p=n;p>=1;p--){
+        //     for(int sum=300;sum>=0;sum--){
+        //         if((sum+pow(p,x))>n) continue;
+        //         int take=dp[p+1][sum+pow(p,x)];
+        //         int non_take=dp[p+1][sum];
+        //         dp[p][sum]=(take+non_take)%mod;
+        //     }
+        // }
+        // return dp[1][0];
     }
 };
