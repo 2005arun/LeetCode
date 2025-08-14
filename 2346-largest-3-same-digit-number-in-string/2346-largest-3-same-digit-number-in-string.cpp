e5b0987e953e3cb50f7a6eed7e12f931e7930ac8
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-14 08:45:31 +0530

    Time: 0 ms (100%), Space: 9 MB (77.37%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        int l=0,r=3;
-        int maxm=-1;
-        while(r<=num.size()){
-            string sub=num.substr(l,r-l);
-            // cout<<sub<<" ";
-            set<char> st;
-            int j=0;
-            while(j<3){
-                st.insert(sub[j]);
-                j++;
-
-            }
-            // cout<<st.size()<<" ";
-            if(st.size()==1){
-                maxm=max(maxm,sub[0]-'0');
-            }
-            l++,r++;
-        }
-        // cout<<maxm;
-        if(maxm==-1) return "";
-        string res="";
-        for(int i=0;i<3;i++){
-            res.push_back((maxm+'0'));
-        }
-        return res;
-
+        // int l=0,r=3;
         // int maxm=-1;
-        // char ch=num[0];
-        // int i=1;
-        // while(i<num.size()){
-        //     int cnt=0;
-        //     while(i<num.size() && num[i]==ch){
-        //         cnt++;
-        //         if(cnt>=3){
-        //             maxm=max(maxm,num[i]-'0');
-        //         }
-        //         i++;
+        // while(r<=num.size()){
+        //     string sub=num.substr(l,r-l);
+        //     // cout<<sub<<" ";
+        //     set<char> st;
+        //     int j=0;
+        //     while(j<3){
+        //         st.insert(sub[j]);
+        //         j++;
+
         //     }
-        //     ch=num[i];
-        //     i++;
+        //     // cout<<st.size()<<" ";
+        //     if(st.size()==1){
+        //         maxm=max(maxm,sub[0]-'0');
+        //     }
+        //     l++,r++;
         // }
-        // cout<<maxm;
+        // // cout<<maxm;
+        // if(maxm==-1) return "";
         // string res="";
         // for(int i=0;i<3;i++){
-        //     res+='7';
+        //     res.push_back((maxm+'0'));
         // }
         // return res;
+
+        int maxm=-1;
+        char ch=num[0];
+        int i=1;
+        while(i<num.size()){
+            int cnt=1;
+            while(i<num.size() && num[i]==ch){
+                cnt++;
+                if(cnt==3){
+                    maxm=max(maxm,num[i]-'0');
+                    // cout<<maxm<<" ";
+                }
+                i++;
+            }
+            ch=num[i];
+            i++;
+        }
+        // cout<<maxm;
+        string res="";
+        if(maxm==-1) return res;
+        for(int i=0;i<3;i++){
+            res+=(maxm+'0');
+        }
+        return res;
     }
 };
