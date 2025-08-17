d690fdd67c450614789e45ccf3620b0390e45dbc
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-17 14:13:25 +0530

    Time: 10 ms (32.04%), Space: 30.5 MB (21.46%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        vector<vector<int>> vec(matrix.size(),vector<int>(matrix[0].size()));
+        for(int i=0;i<matrix.size();i++){
+            vec[i][0]=matrix[i][0];
+        }
+        for(int i=0;i<matrix[0].size();i++){
+            vec[0][i]=matrix[0][i];
+        }
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         cout<<vec[i][j]<<" "; 
+        //     }
+        //     cout<<endl;
+        // }
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][j]==0) {
+                    vec[i][j]=0;
+                    continue;
+                }
+                int minim=min(vec[i-1][j-1],min(vec[i-1][j],vec[i][j-1]));
+                vec[i][j]=minim+1;
+            }
+        }
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         cout<<vec[i][j]<<" "; 
+        //     }
+        //     cout<<endl;
+        // }
+        int sum=0;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                sum+=vec[i][j];
+            }
+        }
+        return sum;
+    }
+};
