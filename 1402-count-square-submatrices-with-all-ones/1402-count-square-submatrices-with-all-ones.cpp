35630ffa8e786486af44b7672c5bfa22ad8f072e
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-08-20 09:06:16 +0530

    Time: 10 ms (32.18%), Space: 30.4 MB (21.64%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -8,28 +8,16 @@ public:
         for(int i=0;i<matrix[0].size();i++){
             vec[0][i]=matrix[0][i];
         }
-        // for(int i=0;i<matrix.size();i++){
-        //     for(int j=0;j<matrix[0].size();j++){
-        //         cout<<vec[i][j]<<" "; 
-        //     }
-        //     cout<<endl;
-        // }
         for(int i=1;i<matrix.size();i++){
             for(int j=1;j<matrix[0].size();j++){
                 if(matrix[i][j]==0) {
                     vec[i][j]=0;
                     continue;
                 }
-                int minim=min(vec[i-1][j-1],min(vec[i-1][j],vec[i][j-1]));
+                int minim=min({vec[i-1][j-1],vec[i-1][j],vec[i][j-1]});
                 vec[i][j]=minim+1;
             }
         }
-        // for(int i=0;i<matrix.size();i++){
-        //     for(int j=0;j<matrix[0].size();j++){
-        //         cout<<vec[i][j]<<" "; 
-        //     }
-        //     cout<<endl;
-        // }
         int sum=0;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
