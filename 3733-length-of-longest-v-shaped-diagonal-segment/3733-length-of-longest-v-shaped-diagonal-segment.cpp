f1ea46f3df4d79028733208181802c76411790e3
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-09-03 18:55:56 +0530

    Time: 1066 ms (33.56%), Space: 121.6 MB (24.91%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> dirs={{1,1},{1,-1},{-1,-1},{-1,1}};
+    // vector<vector<vector<vector<vector<int>>>>> dp;
+    int dp[501][501][2][4][3];
     int recur(int i,int j,vector<vector<int>> &grid,int f,int dir,int val){
+        if(dp[i][j][f][dir][val]!=-1) return dp[i][j][f][dir][val];
         int ni=i+dirs[dir][0];
         int nj=j+dirs[dir][1];
         if(ni<0 || ni>=grid.size() || nj<0 || nj>=grid[0].size() || grid[ni][nj]!=val) return 0;
@@ -10,10 +13,15 @@ public:
         if(f==0){
             take=1+recur(ni,nj,grid,1,(dir+1)%4,2-val);
         }
-        return max(take,non_take);
+        return dp[i][j][f][dir][val]=max(take,non_take);
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         int ans=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        // dp.assign(n,vector<vector<vector<vector<int>>>>(m,
+        // vector<vector<vector<int>>>(2,vector<vector<int>>(4,vector<int>(3,-1)))));
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
