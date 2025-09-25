6d7e0340ea48d49836807da333fbecf9a576eb22
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-09-25 11:05:13 +0530

    Time: 0 ms (100%), Space: 12.5 MB (97.14%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,6 +3,7 @@ public:
     vector<vector<int>> dp;
     int recur(int i,int j,vector<vector<int>> &triangle){
         if(i==triangle.size()-1) return triangle[i][j];
+        if(i>=triangle.size() || j>=triangle.size()) return INT_MAX;
         if(dp[i][j]!=-1) return dp[i][j];
         int top=recur(i+1,j,triangle);
         int topleft=recur(i+1,j+1,triangle);
@@ -11,19 +12,28 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         int m=triangle[n-1].size();
-        dp.assign(n,vector<int>(m,0));
+        // dp.assign(n,vector<int>(m,-1));
         // return recur(0,0,triangle);
 
-        for(int i=0;i<m;i++){
-            dp[n-1][i]=triangle[n-1][i];
-        }
+        // for(int i=0;i<m;i++){
+        //     dp[n-1][i]=triangle[n-1][i];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=0;j<=i;j++){
+        //         int top=dp[i+1][j];
+        //         int topleft=dp[i+1][j+1];
+        //         dp[i][j]=triangle[i][j]+min(top,topleft);
+        //     }
+        // }
+        // return dp[0][0];
+
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                int top=dp[i+1][j];
-                int topleft=dp[i+1][j+1];
-                dp[i][j]=triangle[i][j]+min(top,topleft);
+
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
             }
         }
-        return dp[0][0];
+        return triangle[0][0];
+        
     }
 };
