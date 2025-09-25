da6cd4f3710e48cd73d88f37bebef952fb6d68d7
Arun R S <152052006+2005arun@users.noreply.github.com>
2025-09-25 12:10:57 +0530

    Time: 251 ms (15.64%), Space: 163.3 MB (15.33%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -18,8 +18,17 @@ public:
             return a[0]<b[0];
         });
         int n=pairs.size();
-        dp.assign(n,vector<int>(n,-1));
-        return recur(0,1,pairs,pairs.size())+1;
-        // return 0;
+        dp.assign(n+1,vector<int>(n+1,0));
+        // return recur(0,1,pairs,pairs.size())+1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=1;j--){
+                int take=0;
+                if(pairs[i][1]<pairs[j][0]) take=1+dp[j][j+1];
+                int non_take=max(dp[i][j+1],dp[j][j+1]);
+                dp[i][j]=max(take,non_take);
+            }
+        }
+        return dp[0][1]+1;
     }
 };
